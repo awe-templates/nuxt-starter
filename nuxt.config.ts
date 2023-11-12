25d77266889c046d67ccb13840507c68b0a9bcab
relliv <epsilonelittr@gmail.com>
2023-11-12 21:43:05 +0300

    Add tailwind support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  ssr: true,
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+  css: ["/assets/scss/main.scss"],
+});
---
+++ pages/testa.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="text-2xl text-green-600 underline">
+            Home</h1>
+        <p>My name is {{ name }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const name = ref('John Doe')
+</script>
